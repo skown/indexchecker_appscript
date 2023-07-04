a93e3ab16b6ca3d08c470557e25eb8d4606f5928
Adrian Skowron <38570801+skown@users.noreply.github.com>
2023-07-04 09:18:02 +0200

    Update cellformula.js

--- cellformula.js
+++ cellformula.js
@@ -13,25 +13,3 @@ function loop_check() {
   const sortRange = sheet.getSheetValues(2, 3, lastRow, 1);
   //console.log(lastColumn + 1)
 }
-
-/*
-const url_array = ["https://brainly.com/question/14863564", "https://brainly.com/question/1713545","https://brainly.com/question/30072057", "https://brainly.com/question/30071740"]
-
-const obj_requests = 
-  {
-    'url': 'https://api.dataforseo.com/v3/serp/google/organic/live/advanced',
-    'method': 'POST',
-    'content-type': 'application/json',
-    'headers': {
-      "Authorization": 'Basic ' + Utilities.base64Encode('martyn.skowron@gmail.com' + ':' + 'e9b7273ff40ffc3e')
-    },
-    'muteHttpExceptions': true,
-    "payload": JSON.stringify([{'keyword':"fsd", 'language_code': 'en', 'location_code': 2840}])
-  }
-
-function show_obj() {
-  const payload = obj_requests['payload'];
-  const finished = JSON.parse(payload)
-  console.log(finished[0]['keyword'])
-}
-*/
